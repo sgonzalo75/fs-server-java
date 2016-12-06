854c797f6036b7d4fe29ac15d2bbaa48d26fe3f9
User <User@Internet-PC>
2016-12-06 15:23:41 +0100

    maiolix

---
+++ http-server/src/main/java/xcarpaccio/Nation.java
@@ -0,0 +1,28 @@
+
+package xcarpaccio;
+
+public class Nation 
+{
+	private String  name;
+	private  String code;
+	private  double value;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public double getValue() {
+		return value;
+	}
+	public void setValue(double value) {
+		this.value = value;
+	}
+	
+}
---
+++ http-server/src/main/java/xcarpaccio/NationList.java
@@ -0,0 +1,17 @@
+package xcarpaccio;
+
+import java.util.List;
+
+public class NationList 
+{
+	private  List<Nation> nations;
+
+	public List<Nation> getNations() {
+		return nations;
+	}
+
+	public void setNations(List<Nation> nations) {
+		this.nations = nations;
+	}
+	
+}
