c6c793e32ad59c73ad2697e0afd780598c9d89e8
Nils <nilsjohan.kruse@axa.de>
2016-12-07 10:45:38 +0100

    Add basic cross selling opportunities

--- http-server/src/main/java/xcarpaccio/CrossSeller.java
+++ http-server/src/main/java/xcarpaccio/CrossSeller.java
@@ -10,8 +10,29 @@ public class CrossSeller {
 
     public List<String> getCrossSellingOpportunities(Order order) {
         List<String> crossSelling = new ArrayList<>();
+
+        if (order.cover.equals("PREMIUM")) {
+            crossSelling.add("yoga");
+        }
+
+        if(children(order)) {
+            crossSelling.add("child care");
+        }
+
+        if(order.country.equals("US"))
         crossSelling.add("insurance");
 
         return crossSelling;
+    }
+
+    private boolean children(Order order) {
+        if(order.travellerAges!=null && order.travellerAges.size()>0) {
+            for (String travellerAge : order.travellerAges) {
+                if(Integer.parseInt(travellerAge)<18) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
